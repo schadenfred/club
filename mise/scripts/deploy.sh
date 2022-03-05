82cae3ea1212d56bdbd9d7bd0fea91a0aa396023
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-03-05 14:35:14 -0800

    adds production key

--- mise/scripts/deploy.sh
+++ mise/scripts/deploy.sh
@@ -2,14 +2,12 @@
 
 docker-compose down
 docker-compose -f docker-compose.prod.yml down
-
-docker build -f Dockerfile.production -t handsomefencer/market-local:latest .
-
+docker build -t handsomefencer/club:latest .
 docker-compose -f docker-compose.prod.yml build --no-cache
 docker-compose -f docker-compose.prod.yml up
-docker-compose -f docker-compose.prod.yml && \
-  run --rm app bin/rails db:environment:set RAILS_ENV=production && \
- bin/setup
+#docker-compose -f docker-compose.prod.yml && \
+#  run --rm app bin/rails db:environment:set RAILS_ENV=production && \
+# bin/setup
 #db:setup RAILS_ENV=production
 
 #docker-compose build

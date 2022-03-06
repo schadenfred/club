186398e1aa609f7389d51e2bbfa414ef40b7d1c7
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-03-05 17:02:19 -0800

    before mods

--- mise/scripts/deploy.sh
+++ mise/scripts/deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 docker-compose down
-docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.prod.yml build
+#docker-compose -f docker-compose.prod.yml build
+
+minikube stop
+minikube delete
+minikube start
+
+kompose convert -f docker-compose.prod.yml -o mise/k8s
+
+kubectl create -f mise/k8s/
+kubectl get pods --watch
+
 #docker build -t --no-cache handsomefencer/club:latest .
 #docker-compose -f docker-compose.prod.yml build --no-cache
-docker-compose -f docker-compose.prod.yml up
+#docker-compose -f docker-compose.prod.yml up
 #docker-compose -f docker-compose.prod.yml && \
 #  run --rm app bin/rails db:environment:set RAILS_ENV=production && \
 # bin/setup

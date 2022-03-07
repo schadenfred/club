9250840a5cb8633c927ba8519418a2fd77898cbd
Fred Schoeneman <fred.schoeneman@gmail.com>
2022-03-06 17:17:28 -0800

    Final k8s configuration for tls on do

--- mise/scripts/deploy.sh
+++ mise/scripts/deploy.sh
@@ -2,8 +2,7 @@
 
 #minikube stop
 #minikube delete
-
-#doctl kubernetes cluster kubeconfig save 410c65eb-8e10-440a-9540-d185e41d6d02
+#minikube start
 #docker-compose down
 #docker-compose -f docker-compose.prod.yml down
 
@@ -12,20 +11,13 @@
 #docker push handsomefencer/club:latest
 
 #docker-compose -f docker-compose.prod.yml up --build
+doctl kubernetes cluster kubeconfig save f0046950-e72c-4ef0-91a9-53b2f42f1c61
+
 
 rm -rf mise/k8s/*
 kompose convert -f docker-compose.prod.yml -o mise/k8s
 cp -r mise/backup/* mise/k8s/
-#minikube start
 
 kubectl apply -f mise/k8s/
 kubectl get pods --watch
 
-#docker-compose -f docker-compose.prod.yml build --no-cache
-#docker-compose -f docker-compose.prod.yml up
-#docker-compose -f docker-compose.prod.yml && \
-#  run --rm app bin/rails db:environment:set RAILS_ENV=production && \
-# bin/setup
-#db:setup RAILS_ENV=production
-
-#docker-compose build
